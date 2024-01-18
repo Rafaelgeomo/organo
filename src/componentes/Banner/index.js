e4253c821ee9a8e09c72e128c7d60a2ca703522e
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2024-01-18 17:02:03 -0300

    Merge branch 'master' of https://github.com/Rafaelgeomo/organo

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+function Banner(){
+  //JSX - Parece HTML mas é JSX
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+    </header>
+  )
+
+}
+
+export default Banner
+
+
