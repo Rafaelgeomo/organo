e83a117052b11514537979981ecd10a72e688cfd
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2024-04-02 16:43:44 -0300

    componente rodape

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,14 +1,9 @@
-import './Banner.css'
-function Banner(){
-  //JSX - Parece HTML mas é JSX
-  return (
-    <header className="banner">
-      <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
-    </header>
-  )
+import './banner.css'
 
+const Banner = () => {
+    return (<header className="cabecalho">
+        <img src='/imagens/banner.png' alt='Logo do Organo'/>
+    </header>)
 }
 
 export default Banner
-
-
